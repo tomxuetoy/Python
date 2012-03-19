03e5c9592d41f6a5e74fcfd3d815c54e1664e71a
Tom Xue <tomxue026@gmail.com>
2012-03-19 04:22:02 +0000

    to show basic file operations in Python: raw_input open writelines write close

---
+++ ex3.py
@@ -0,0 +1,23 @@
+﻿# Let's create a file and write it to disk.
+filename = "test.dat"
+# Let's create some data:
+namelist = []
+while True:
+    name = raw_input("Enter a name:")
+    if name:
+	    namelist.append(name)
+    else:
+	    break
+
+# Create a file object:
+# in "write" mode
+FILE = open(filename,"w")
+
+# Write all the lines at once:
+FILE.writelines(namelist)
+    
+# Alternatively write them one by one:
+for name in namelist:
+    FILE.write(name)
+    
+FILE.close()
