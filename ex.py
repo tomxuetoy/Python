68fab733e1156b76ab7c182be6272e320bc9ea44
Tom Xue <tomxue026@gmail.com>
2012-03-24 23:43:00 +0800

    to show how to make a self-defined exception

--- ex.py
+++ ex.py
@@ -1,1 +1,6 @@
-raise IndexError
+class MyException(Exception):pass # pass: define a empty class
+try:
+    #some code here
+    raise MyException             # it is empty, so it won't be raised in fact
+except MyException:
+    print "MyException encoutered"
